4f34c48e47bfc1c2600c63c472f922d9582ca194
hakim-b <hakimbouabdellah02@gmail.com>
2024-08-19 22:06:18 -0400

    started creating tabs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,27 @@
-function Home() {
-  return <></>;
-}
+import fs from "fs";
+import path from "path";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "~/components/ui/tabs";
 
-export default Home;
+// Server Component to read subdirectories and render tabs
+export default async function Home() {
+  const directoryPath = path.join(process.cwd(), "examotron_backup");
+  const subdirectories = fs
+    .readdirSync(directoryPath, { withFileTypes: true })
+    .filter((dirent) => dirent.isDirectory())
+    .map((dirent) => dirent.name);
+
+  return (
+    <Tabs defaultValue={subdirectories[0]} className="w-[400px]">
+      <TabsList className="grid w-full grid-cols-12 gap-28">
+        {subdirectories.map((subdir) => (
+          <TabsTrigger key={subdir} value={subdir}>
+            {subdir}
+          </TabsTrigger>
+        ))}
+      </TabsList>
+      {subdirectories.map((subdir) => (
+        <TabsContent key={subdir} value={subdir}></TabsContent>
+      ))}
+    </Tabs>
+  );
+}
