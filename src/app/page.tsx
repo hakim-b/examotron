348f61c474d61c2d57e7ed4a95e3488e77f1a3af
hakim-b <hakimbouabdellah02@gmail.com>
2024-08-21 14:31:04 -0400

    having trouble displaying subdirectries of subdirectories

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,45 @@
 import fs from "fs";
 import path from "path";
+import { Tree, File, Folder } from "~/components/magicui/file-tree";
+import Navbar from "~/components/navbar";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "~/components/ui/tabs";
+
+type DirElement = {
+  id: string;
+  name: string;
+  isSelectable: boolean;
+  children?: DirElement[];
+};
+
+// Function to generate a tree structure
+const generateTree = (directoryPath: string, baseId = ""): DirElement[] => {
+  const elements: DirElement[] = [];
+  const dirItems = fs.readdirSync(directoryPath, { withFileTypes: true });
+
+  dirItems.forEach((item, index) => {
+    const itemPath = path.join(directoryPath, item.name);
+    const itemId = `${baseId}${index}`;
+
+    if (item.isDirectory()) {
+      const children = generateTree(itemPath, `${itemId}-`);
+
+      elements.push({
+        id: itemId,
+        isSelectable: true,
+        name: item.name,
+        children,
+      });
+    } else {
+      elements.push({
+        id: itemId,
+        isSelectable: true,
+        name: item.name,
+      });
+    }
+  });
+
+  return elements;
+};
 
 // Server Component to read subdirectories and render tabs
 export default async function Home() {
@@ -11,17 +50,41 @@ export default async function Home() {
     .map((dirent) => dirent.name);
 
   return (
-    <Tabs defaultValue={subdirectories[0]} className="w-[400px]">
-      <TabsList className="grid w-full grid-cols-12 gap-28">
-        {subdirectories.map((subdir) => (
-          <TabsTrigger key={subdir} value={subdir}>
-            {subdir}
-          </TabsTrigger>
-        ))}
-      </TabsList>
-      {subdirectories.map((subdir) => (
-        <TabsContent key={subdir} value={subdir}></TabsContent>
-      ))}
-    </Tabs>
+    <>
+      <Navbar />
+      <Tabs defaultValue={subdirectories[0]} className="w-full">
+        <TabsList className="grid w-full grid-cols-12">
+          {subdirectories.map((subdir) => (
+            <TabsTrigger key={subdir} value={subdir}>
+              {subdir}
+            </TabsTrigger>
+          ))}
+        </TabsList>
+        {subdirectories.map((subdir) => {
+          const elements = generateTree(path.join(directoryPath, subdir));
+          return (
+            <TabsContent key={subdir} value={subdir}>
+              <div className="relative flex h-[300px] w-1/2 flex-col items-center justify-center overflow-hidden rounded-lg border bg-background md:shadow-xl">
+                <Tree
+                  className="p-2 overflow-hidden rounded-md bg-background"
+                  initialExpandedItems={["1"]}
+                  elements={elements}
+                >
+                  {elements.map((element) => (
+                    <Folder key={element.id} element={element.name} value={element.id}>
+                      {element.children?.map((child) => (
+                        <File key={child.id} value={child.id}>
+                          <p>{child.name}</p>
+                        </File>
+                      ))}
+                    </Folder>
+                  ))}
+                </Tree>
+              </div>
+            </TabsContent>
+          );
+        })}
+      </Tabs>
+    </>
   );
 }
