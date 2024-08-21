d49178c04d769d9c136825ca21df207ec81f5a97
hakim-b <hakimbouabdellah02@gmail.com>
2024-08-21 19:22:30 -0400

    files wont download

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,53 +1,70 @@
 import fs from "fs";
+import Link from "next/link";
 import path from "path";
-import { Tree, File, Folder } from "~/components/magicui/file-tree";
+import {
+  Tree,
+  File,
+  Folder,
+  TreeViewElement,
+} from "~/components/magicui/file-tree";
 import Navbar from "~/components/navbar";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "~/components/ui/tabs";
 
-type DirElement = {
-  id: string;
-  name: string;
-  isSelectable: boolean;
-  children?: DirElement[];
-};
-
-// Function to generate a tree structure
-const generateTree = (directoryPath: string, baseId = ""): DirElement[] => {
-  const elements: DirElement[] = [];
-  const dirItems = fs.readdirSync(directoryPath, { withFileTypes: true });
-
-  dirItems.forEach((item, index) => {
-    const itemPath = path.join(directoryPath, item.name);
-    const itemId = `${baseId}${index}`;
-
-    if (item.isDirectory()) {
-      const children = generateTree(itemPath, `${itemId}-`);
-
-      elements.push({
-        id: itemId,
-        isSelectable: true,
-        name: item.name,
-        children,
-      });
-    } else {
-      elements.push({
-        id: itemId,
-        isSelectable: true,
-        name: item.name,
-      });
-    }
-  });
-
-  return elements;
-};
-
-// Server Component to read subdirectories and render tabs
 export default async function Home() {
   const directoryPath = path.join(process.cwd(), "examotron_backup");
   const subdirectories = fs
     .readdirSync(directoryPath, { withFileTypes: true })
     .filter((dirent) => dirent.isDirectory())
     .map((dirent) => dirent.name);
+
+  const generateTree = (directoryPath: string, baseId = "") => {
+    const elements: TreeViewElement[] = [];
+    const dirItems = fs.readdirSync(directoryPath, { withFileTypes: true });
+
+    dirItems.forEach((item, index) => {
+      const itemPath = path.join(directoryPath, item.name);
+      const itemId = `${baseId}${index}`;
+
+      if (item.isDirectory()) {
+        const children = generateTree(itemPath, `${itemId}-`);
+        elements.push({
+          id: itemId,
+          isSelectable: true,
+          name: item.name,
+          children,
+        });
+      } else {
+        elements.push({
+          id: itemId,
+          isSelectable: true,
+          name: item.name,
+        });
+      }
+    });
+
+    return elements;
+  };
+
+  const renderTreeElements = (elements: TreeViewElement[]) => {
+    return elements.map((element) =>
+      element.children && element.children.length > 0 ? (
+        <Folder key={element.id} element={element.name} value={element.id}>
+          {renderTreeElements(element.children)}
+        </Folder>
+      ) : (
+        <File key={element.id} value={element.id}>
+          <Link
+            href={element.name}
+            download
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            {element.name}
+          </Link>
+        </File>
+      ),
+    );
+  };
 
   return (
     <>
@@ -66,19 +83,11 @@ export default async function Home() {
             <TabsContent key={subdir} value={subdir}>
               <div className="relative flex h-[300px] w-1/2 flex-col items-center justify-center overflow-hidden rounded-lg border bg-background md:shadow-xl">
                 <Tree
-                  className="p-2 overflow-hidden rounded-md bg-background"
+                  className="overflow-hidden rounded-md bg-background p-2"
                   initialExpandedItems={["1"]}
                   elements={elements}
                 >
-                  {elements.map((element) => (
-                    <Folder key={element.id} element={element.name} value={element.id}>
-                      {element.children?.map((child) => (
-                        <File key={child.id} value={child.id}>
-                          <p>{child.name}</p>
-                        </File>
-                      ))}
-                    </Folder>
-                  ))}
+                  {renderTreeElements(elements)}
                 </Tree>
               </div>
             </TabsContent>
