773f713be753a683448f191a77624d660b9e4d9b
hakim-b <hakimbouabdellah02@gmail.com>
2024-09-02 21:45:33 -0400

    *sigh*

--- src/lib/upstash.ts
+++
@@ -1,8 +0,0 @@
-import { serverEnv } from "~/env/server";
-import { Index } from "@upstash/vector";
-import { ExamotronFile } from "~/types";
-
-export const upstashIndex = new Index<ExamotronFile>({
-  url: serverEnv.UPSTASH_VECTOR_REST_URL,
-  token: serverEnv.UPSTASH_VECTOR_REST_TOKEN,
-});
