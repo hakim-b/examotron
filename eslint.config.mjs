53ca43d51f349a6134251a3359cc070730c83fbc
hakim-b <hakimbouabdellah02@gmail.com>
2025-05-02 21:19:11 -0400

    try again

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,15 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.config({
+    extends: ["next/core-web-vitals", "next/typescript"],
+    rules: {
+      "react-hooks/exhaustive-deps": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-empty-object-type": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+    },
+  }),
 ];
 
 export default eslintConfig;
