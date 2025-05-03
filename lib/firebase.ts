fd1e0e5435473c35d9bd126603e03a223591a9ef
hakim-b <hakimbouabdellah02@gmail.com>
2025-05-02 20:41:29 -0400

    new repo

---
+++ lib/firebase.ts
@@ -0,0 +1,15 @@
+import { FirebaseOptions, getApp, getApps, initializeApp } from "firebase/app";
+import { getStorage } from "firebase/storage";
+import { clientEnv } from "@/env/client";
+
+const firebaseConfig: FirebaseOptions = {
+  apiKey: clientEnv.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: clientEnv.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: clientEnv.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: clientEnv.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: clientEnv.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: clientEnv.NEXT_PUBLIC_FIREBASE_APP_ID,
+};
+
+const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
+export const storage = getStorage(app);
